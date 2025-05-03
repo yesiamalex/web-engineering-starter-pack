c0e967bc21a7fda5ae318c4a7a5a411da20071ff
aleksZf <zcfigueroa@up.edu.ph>
2025-05-03 17:49:48 +0800

    Completed logout functionality

--- LE-2/backend/budget_project/accounts/urls.py
+++ LE-2/backend/budget_project/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', TokenBlacklistView.as_view(), name='logout'),
-    path('me/', AccountInfoView.as_view(), name='account_info'),  # New endpoint
+    path('me/', AccountInfoView.as_view(), name='account_info'),
 ]
